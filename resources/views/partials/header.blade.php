8dfbce808158335711348c49c38276a7db80f636
Philip Loa <boyet007@gmail.com>
2018-01-11 21:15:58 +0700

    Penambahan icon shopping dan user

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,12 +6,11 @@
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Shopping Cart <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Shopping Cart <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          User Account
-        </a>
+         <i class="fa fa-user-o" aria-hidden="true"></i> User Account</a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
