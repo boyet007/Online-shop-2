b6e89ee77c50fb7155b82948f220acf1d4673022
Philip Loa <boyet007@gmail.com>
2018-01-11 21:50:54 +0700

    set image thumbail di index

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,5 +5,14 @@ Laravel Shopping Cart
 @endsection
 
 @section('content')
-<h1>It works !!!</h1>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Product Title</h5>
+    <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  </div>
+  <div class="card-body">
+    <a href="#" class="btn btn-success">Card link</a>
+  </div>
+</div>
 @endsection
