392e873282f4a5d85bcf6b58a72f316350366d63
Philip Loa <boyet007@gmail.com>
2018-01-12 04:21:22 +0700

    edit thumbnail card

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,12 +5,14 @@ Laravel Shopping Cart
 @endsection
 
 @section('content')
+<div class="row">
 <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
+  <img class="card-img-top img-responsive" src="https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg" alt="Card image cap">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Product Title</h5>
+    <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore.</p>
+    <a href="#" class="btn btn-primary pull-right">Go somewhere</a>
   </div>
+</div>
 </div>
 @endsection
