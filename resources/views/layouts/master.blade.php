99c7ee374be8130a8f1c80fba2c6140e279b1ee4
Philip Loa <boyet007@gmail.com>
2018-01-11 21:24:59 +0700

    Penambahan kelas 'container' pada yield content

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,9 @@
 </head>
 <body>
 	@include('partials.header')
-	@yield('content')
+	<div class="container">
+		@yield('content')
+	</div>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
