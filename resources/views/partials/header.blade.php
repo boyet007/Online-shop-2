b9666c0993ce9ad601db5421bf14b6935152d851
Philip Loa <boyet007@gmail.com>
2018-01-12 04:47:03 +0700

    Perbaikan html di header blade

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,11 +11,13 @@
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
          <i class="fa fa-user-o" aria-hidden="true"></i> User Account</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      </nav>
+    </ul>
+  </div>
+</nav>
