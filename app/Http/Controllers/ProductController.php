c7bd7cba732dc6b22c5d2130378cc023e727382b
Philip Loa <boyet007@gmail.com>
2018-01-12 16:26:34 +0700

    buat function getIndex di ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function getIndex(){
+    	@products = Product::all();
+    	return view('shop.index', ['products' => $products]);
+    }
 }
