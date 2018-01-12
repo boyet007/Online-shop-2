d9b479e90ffb2ab67335b56e9fbef7d69dcea69a
Philip Loa <boyet007@gmail.com>
2018-01-12 15:53:00 +0700

    Buat data dump

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -11,6 +11,57 @@ class ProductTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $product = new \App\Product([
+        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
+        	'title' => 'Harry Potter',
+        	'description' => 'Super cool - at least at child.', 
+        	'price' => 10
+
+        ]);
+        $product->save();
+                $product = new \App\Product([
+        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
+        	'title' => 'Harry Potter',
+        	'description' => 'Super cool - at least at child.', 
+        	'price' => 10
+
+        ]);
+        $product->save();
+                $product = new \App\Product([
+        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
+        	'title' => 'Harry Potter',
+        	'description' => 'Super cool - at least at child.', 
+        	'price' => 10
+
+        ]);
+        $product->save();
+                $product = new \App\Product([
+        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
+        	'title' => 'Harry Potter',
+        	'description' => 'Super cool - at least at child.', 
+        	'price' => 10
+
+        ]);
+        $product->save();
+
+        $product->save();
+                $product = new \App\Product([
+        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
+        	'title' => 'Harry Potter',
+        	'description' => 'Super cool - at least at child.', 
+        	'price' => 10
+
+        ]);
+        $product->save();
+        
+        $product->save();
+                $product = new \App\Product([
+        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
+        	'title' => 'Harry Potter',
+        	'description' => 'Super cool - at least at child.', 
+        	'price' => 10
+
+        ]);
+        $product->save();
     }
 }
