700e14c64feb15ba7c934477f61461e6977f1209
Philip Loa <boyet007@gmail.com>
2018-01-13 05:38:32 +0700

    set product variable diindex view

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -10,11 +10,11 @@ Laravel Shopping Cart
 	@foreach($productChunk as $product)
 	<div class="col-sm-6 col-md-4">
 		<div class="card" style="width: 18rem;">
-			<img class="card-img-top img-responsive" src="https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg" alt="Card image cap">
+			<img class="card-img-top img-responsive" src="{{ $product->imagePath }}">
 			<div class="card-body">
-				<h5 class="card-title">Product Title</h5>
-				<p class="card-text description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore.</p>
-				<div class="pull-left price">$12</div>
+				<h5 class="card-title">{{ $product->title }}</h5>
+				<p class="card-text description">{{ $product->description }}</p>
+				<div class="pull-left price">{{ $product->price }}</div>
 				<a href="#" class="btn btn-success pull-right">Add to Cart</a>
 			</div>
 		</div>
