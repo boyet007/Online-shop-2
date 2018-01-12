aa66aa7e2f6880a054cf46406504b896872309ae
Philip Loa <boyet007@gmail.com>
2018-01-12 16:04:12 +0700

    Revisi untuk model
    set integer untuk kolom price
    buang semua table di database phpmyadmin
    di command ketik : php artisan migrate (membuat table baru)
    php artisan db:seed -> mengisi tabel dari ProductTableSeeder

--- database/migrations/2018_01_12_081255_create_products_table.php
+++ database/migrations/2018_01_12_081255_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->string('imagePath');
             $table->string('title');
             $table->text('description');
-            $table->text('price');
+            $table->integer('price');
 
 
         });
