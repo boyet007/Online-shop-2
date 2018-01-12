134ea4e2dc6592b6789255d63a874240dab89f80
Philip Loa <boyet007@gmail.com>
2018-01-13 05:01:32 +0700

    Revisi Product Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function getIndex(){
-    	@products = Product::all();
+    	$products = Product::all();
     	return view('shop.index', ['products' => $products]);
     }
 }
