9f4e551d7ed1b8ca26e3e88cb72cbb886d50281e
Philip Loa <boyet007@gmail.com>
2018-01-15 21:05:02 +0700

    Revisi auth UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,44 +10,49 @@ use Auth;
 
 class UserController extends Controller
 {
-    public function getSignup(){
-    	return view('user.signup');
-    }
+	public function getSignup(){
+		return view('user.signup');
+	}
 
-    public function postSignup(Request $request){
-    	$this->validate($request, [
-    		'email' => 'email|required|unique:users',
-    		'password' => 'required|min:4'
-    	]);
+	public function postSignup(Request $request){
+		$this->validate($request, [
+			'email' => 'email|required|unique:users',
+			'password' => 'required|min:4'
+		]);
 
-    	$user = new User([
-    		'email' => $request->input('email'), 
-    		'password' => bcrypt($request->input('password'))
-    	]);
+		$user = new User([
+			'email' => $request->input('email'), 
+			'password' => bcrypt($request->input('password'))
+		]);
 
-    	$user->save();
+		$user->save();
 
-    	return redirect()->route('product.index');
-    }
+		return redirect()->route('product.index');
+	}
 
-    public function getSignin(){
-    	return view ('user.signin');
-    }
+	public function getSignin(){
+		return view ('user.signin');
+	}
 
-    public function postSignin(Request $request){
-    	$this->validate($request, [
-    		'email' => 'required',
-    		'password' => 'required'
-    	]);	
+	public function postSignin(Request $request){
+		$this->validate($request, [
+			'email' => 'required',
+			'password' => 'required'
+		]);	
 
-        if(Auth::attempt([
-    		'email' => $request -> input('email')]),
-    		'password' => $request -> input('password')])){
-				return redirect()->route('user.profile');			
+		if(Auth::attempt([
+			'email' => $request->input('email'), 'password' => $request->input('password')])){
+			return redirect()->route('user.profile');			
 		} else {
 			return redirect()->back();
 		}
+	}
 
+	public function getProfile(){
+		return view('user.profile');
+	}
 
-    }
+	public function getLogout(){
+		Auth::logout();
+	}
 }
