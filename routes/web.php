7227c625e25b56eaffb015ac0382b6a5b3220bef
Philip Loa <boyet007@gmail.com>
2018-01-16 12:07:25 +0700

    Auto stash before merge of "master" and "origin/master"
    Revisi web routing untuk grup user

--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,36 @@ Route::get('/', [
 
 ]);
 
-Route::get('/signup', [
-	'uses' => 'UserController@getSignup', 
-	'as' => 'user.signup'
-]);
+Route::group(['prefix' => 'user'] , function({
 
-Route::post('/signup', [
-	'uses' => 'UserController@postSignup', 
-	'as' =>	'user.signup'
-]);
+	Route::get('/signup', [
+		'uses' => 'UserController@getSignup', 
+		'as' => 'user.signup'
+	]);
 
-Route::get('/signin', [
-	'uses' => 'UserController@getSignin', 
-	'as' => 'user.signin'
-]);
+	Route::post('/signup', [
+		'uses' => 'UserController@postSignup', 
+		'as' =>	'user.signup'
+	]);
 
-Route::post('/signin', [
-	'uses' => 'UserController@postSignin', 
-	'as' =>	'user.signin'
-]);
+	Route::get('/signin', [
+		'uses' => 'UserController@getSignin', 
+		'as' => 'user.signin'
+	]);
 
-Route::get('/user/profile', [
-	'uses' => 'UserController@getProfile',
-	'as' => 'user.profile'
-]);
+	Route::post('/signin', [
+		'uses' => 'UserController@postSignin', 
+		'as' =>	'user.signin'
+	]);
 
-Route::get('/user/logout', [
-	'uses' => 'UserController@getLogout',
-	'as' => 'user.logout'
-]);
+	Route::get('/user/profile', [
+		'uses' => 'UserController@getProfile',
+		'as' => 'user.profile'
+	]);
 
+	Route::get('/user/logout', [
+		'uses' => 'UserController@getLogout',
+		'as' => 'user.logout'
+	]);
 
+}));
