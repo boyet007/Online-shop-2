4a4d7330fb9c3dac14f7b3bd1485998fdd25e94c
Philip Loa <boyet007@gmail.com>
2018-01-16 14:45:44 +0700

    Edit table seeder

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -20,35 +20,35 @@ class ProductTableSeeder extends Seeder
         ]);
         $product->save();
                 $product = new \App\Product([
-        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
-        	'title' => 'Harry Potter',
-        	'description' => 'Super cool - at least at child.', 
+        	'imagePath' => 'https://yt3.ggpht.com/-tSSS2CHTsgc/AAAAAAAAAAI/AAAAAAAAAAA/Oz6w4Mk4Dq0/s900-c-k-no-mo-rj-c0xffffff/photo.jpg', 
+        	'title' => 'Donal Duck',
+        	'description' => 'Funny duck in the history', 
+        	'price' => 11
+
+        ]);
+        $product->save();
+                $product = new \App\Product([
+        	'imagePath' => 'https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png', 
+        	'title' => 'Mickey_Mouse',
+        	'description' => 'Funny mouse in the history', 
         	'price' => 10
 
         ]);
         $product->save();
                 $product = new \App\Product([
-        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
-        	'title' => 'Harry Potter',
-        	'description' => 'Super cool - at least at child.', 
-        	'price' => 10
-
-        ]);
-        $product->save();
-                $product = new \App\Product([
-        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
-        	'title' => 'Harry Potter',
-        	'description' => 'Super cool - at least at child.', 
-        	'price' => 10
+        	'imagePath' => 'http://english-draw.salabun.com/img/654_0.jpg', 
+        	'title' => 'Gufi',
+        	'description' => 'Funny dog in history', 
+        	'price' => 7
 
         ]);
         $product->save();
 
         $product->save();
                 $product = new \App\Product([
-        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
-        	'title' => 'Harry Potter',
-        	'description' => 'Super cool - at least at child.', 
+        	'imagePath' => 'http://1.bp.blogspot.com/-LEVK4pOvopo/VjPYlDRtEfI/AAAAAAAAFSo/JcVcK-ITNKE/s1600/usro.jpg', 
+        	'title' => 'Si Usro',
+        	'description' => 'Temen si kinoi.', 
         	'price' => 10
 
         ]);
@@ -56,9 +56,9 @@ class ProductTableSeeder extends Seeder
         
         $product->save();
                 $product = new \App\Product([
-        	'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/71Ui-NwYUmL.jpg', 
-        	'title' => 'Harry Potter',
-        	'description' => 'Super cool - at least at child.', 
+        	'imagePath' => 'https://kemptonexpress.co.za/wp-content/uploads/sites/30/2016/07/tom-and-jerry.jpg', 
+        	'title' => 'Tom and Jerry',
+        	'description' => 'funny cat.', 
         	'price' => 10
 
         ]);
