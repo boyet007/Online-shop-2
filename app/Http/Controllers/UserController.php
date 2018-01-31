fe0a24b9762147c8fd87fab82cfec6208836d285
Philip Loa <boyet007@gmail.com>
2018-01-31 14:05:21 +0700

    Edit user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Auth;
+use Illuminate\Support\Facades\Input;
 
 class UserController extends Controller
 {
@@ -24,5 +26,34 @@ class UserController extends Controller
         $user->save();
 
         return redirect()->route('product.index');
+    }
+
+    public function getSignin(){
+        return view('user.signin');
+    }
+
+    public function postSignin(Request $request){
+        $this->validate($request, [
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        $data = array(
+            'email' => Input::get('email'),
+            'password' => Input::get('password')
+        );
+        print_r($data);
+
+        dd(Auth::attempt($data));
+
+        if(Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])){
+            return redirect()->route('user.profile');
+        }
+
+        return redirect()->back();
+    }
+
+    public function getProfile(){
+        return view('user.profile');
     }
 }
