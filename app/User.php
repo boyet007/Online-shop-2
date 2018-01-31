8ebe7ab93182ce4ea3f0edaaa635c30f878e7dd7
Philip Loa <boyet007@gmail.com>
2018-01-31 10:24:23 +0700

    Revisi user to default model user

--- app/User.php
+++ app/User.php
@@ -2,11 +2,28 @@
 
 namespace App;
 
-//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    protected $fillable=['email', 'password'];
-    protected $hidden = ['password', 'remember_token'];
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
