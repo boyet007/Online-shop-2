c2643541abd755ba8468f3a52909667e779b17b3
Philip Loa <boyet007@gmail.com>
2018-01-31 10:27:04 +0700

    Create signup view

--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Philip
- * Date: 1/31/2018
- * Time: 9:30 AM
- */
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        <h1>Sign Up</h1>
+        @if(count($errors) > 0)
+            @foreach($errors->all() as $error)
+                <div class="alert alert-danger">
+                    <p>{{ $error }}</p>
+                </div>
+            @endforeach
+        @endif
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="email">E-Mail</label>
+                <input type="text" name="email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <button type="submit">Sign Up</button>
+        </form>
+    </div>
+</div>
