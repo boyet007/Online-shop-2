da079011c4e5fe11fb4dfce1444cc6869356aef8
Philip Loa <boyet007@gmail.com>
2018-01-31 11:15:05 +0700

    Revisi header layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,12 +10,11 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         <i class="fa fa-user-o" aria-hidden="true"></i> User Account</a>
+         <i class="fa fa-user-o" aria-hidden="true"></i> User Management</a>
          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" href="{{ route('user.signup') }}">Signup</a>
+                <a class="dropdown-item" href="#">Signin</a>
+
         </div>
       </li>
     </ul>
