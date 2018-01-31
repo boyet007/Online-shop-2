ca0d3dbf26efb98394866c258b9e1f04086b2da2
Philip Loa <boyet007@gmail.com>
2018-01-31 20:32:59 +0700

    Revisi

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,18 @@ Route::get('/', [
     'as' => 'product.index'
 ]);
 
-Route::get('/signup',[
-    'uses' => 'UserController@getSignup',
-    'as' => 'user.signup'
-]);
+
 
 
 
 Route::group(['prefix' => 'user'], function(){
+    Route::get('/signup',[
+        'uses' => 'UserController@getSignup',
+        'as' => 'user.signup'
+    ]);
+
     Route::post('/signup',[
-        'uses' => 'Usercontroller@postSignup',
+        'uses' => 'UserController@postSignup',
         'as' =>'user.signup'
     ]);
 
