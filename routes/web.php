e81790f7ad9c9fb6abe88beee8a5b92b5223ab5c
Philip Loa <boyet007@gmail.com>
2018-01-31 11:16:03 +0700

    Revisi routing untuk product index dan add route user.signup

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@
 
 Route::get('/', [
     'uses' => 'ProductController@getIndex',
-    'as' => 'product.getIndex'
+    'as' => 'product.index'
+]);
+
+Route::get('/signup',[
+    'uses' => 'UserController@getSignup',
+    'as' => 'user.signup'
+]);
+
+Route::post('/signup',[
+    'uses' => 'Usercontroller@postSignup',
+    'as' =>'user.signup'
 ]);
 
